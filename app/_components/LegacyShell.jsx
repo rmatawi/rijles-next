14b66cc0d36495b8c4d362d3e0e37aaa3f087354
rianmatawi <rianmatawi@gmail.com>
2026-03-08 14:14:04 -0300

    a01

--- app/_components/LegacyShell.jsx
+++ app/_components/LegacyShell.jsx
@@ -1,6 +1,8 @@
-"use client";
+ "use client";
 
-import LegacyApp from "../LegacyApp";
+import dynamic from "next/dynamic";
+
+const LegacyApp = dynamic(() => import("../LegacyApp"), { ssr: false });
 
 export default function LegacyShell() {
   return <LegacyApp />;
